6d1ceea6f509ccbe66d14d3d26c1c0f6de65a6a0
venkatapraveena123neti <41039346+venkatapraveena123neti@users.noreply.github.com>
2018-08-06 12:32:39 +0530

    Update WalletapplicationspringApplication.java

--- walletapplicationspring/src/main/java/com/cg/walletapplication/WalletapplicationspringApplication.java
+++ walletapplicationspring/src/main/java/com/cg/walletapplication/WalletapplicationspringApplication.java
@@ -2,7 +2,16 @@ package com.cg.walletapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//------------------------    Wallet Application --------------------------
+		/*******************************************************************************************************
+		 - Class Name	        :	         <WalletapplicationspringApplication>
+		 - Input Parameters	:	<null>
+		 - Return Type		:	<null> <data type>
+		 - Throws			:  	<Application_name>Exception
+		 - Author			:	<Neti_venkata_praveena>
+		 - Creation Date	:	06/08/2018
+		 - Description		:	Main class which runs the spring application
+		 ********************************************************************************************************/
 @SpringBootApplication
 public class WalletapplicationspringApplication {
 
