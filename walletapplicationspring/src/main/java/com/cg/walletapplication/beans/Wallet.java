65cee6d1d3f05173c938007435aee1cfb2851b5b
venkatapraveena123neti <41039346+venkatapraveena123neti@users.noreply.github.com>
2018-08-06 12:22:46 +0530

    Update Wallet.java

--- walletapplicationspring/src/main/java/com/cg/walletapplication/beans/Wallet.java
+++ walletapplicationspring/src/main/java/com/cg/walletapplication/beans/Wallet.java
@@ -5,7 +5,16 @@ import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-
+            //------------------------    Wallet Application --------------------------
+		/*******************************************************************************************************
+		 - Class Name	        :	         <Wallet>
+		 - Input Parameters	:	<null>
+		 - Return Type		:	<null> <data type>
+		 - Throws			:  	<Application_name>Exception
+		 - Author			:	<Neti_venkata_praveena>
+		 - Creation Date	:	06/08/2018
+		 - Description		:	bean class to connect to database embedded to customer class
+		 ********************************************************************************************************/
 @Embeddable
 public class Wallet implements Serializable {
 	private static final long serialVersionUID = 1156737872192523305L;
