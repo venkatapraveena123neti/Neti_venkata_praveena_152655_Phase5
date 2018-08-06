2eb81d4c8d27108c397b240ccd4e4c53a15f9115
venkatapraveena123neti <41039346+venkatapraveena123neti@users.noreply.github.com>
2018-08-06 12:30:01 +0530

    Update IWalletRepo.java

--- walletapplicationspring/src/main/java/com/cg/walletapplication/repo/IWalletRepo.java
+++ walletapplicationspring/src/main/java/com/cg/walletapplication/repo/IWalletRepo.java
@@ -4,7 +4,16 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cg.walletapplication.beans.Customer;
-
+  //------------------------    Wallet Application --------------------------
+		/*******************************************************************************************************
+		 - Interface Name	        :	         <IWalletRepo>
+		 - Input Parameters	:	<null>
+		 - Return Type		:	<null> <data type>
+		 - Throws			:  	<Application_name>Exception
+		 - Author			:	<Neti_venkata_praveena>
+		 - Creation Date	:	06/08/2018
+		 - Description		:	interface extends Crud to perform operations
+		 ********************************************************************************************************/
 @Repository("walletrepo")
 public interface IWalletRepo extends CrudRepository<Customer, String> {
 
